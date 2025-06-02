1df0ea85fd9226e8c3b3c0a3fe49c44c8e4f558e
Adharsh Shokkalingam <adharsh@Adharshs-MacBook-Air.local>
2025-06-02 17:23:02 -0600

    testing

--- Database.c
+++ Database.c
@@ -54,6 +54,8 @@ void close_input_buffer(InputBuffer* input_buffer){
     free(input_buffer); 
 }
 
+
+// Main function 
 int main(int argc, char* argv[]){
     InputBuffer* input_buffer = new_input_buffer();
 
