f250a734bf31085b3e5fbb0893f41e65aa8fcb1d
Adharsh Shokkalingam <adharsh@Adharshs-MacBook-Air.local>
2025-06-03 23:34:55 -0600

    added the struct and defined stuff

--- Database.c
+++ Database.c
@@ -30,8 +30,18 @@ typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATMENT}PrepareResult;
 
 typedef enum {STATEMENT_INSERT, STATEMENT_SELECT}StatementType;
 
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+typedef struct {
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+}Row;
+
+
 typedef struct{
     StatementType type;
+    Row row_to_insert;
 }Statement;
 
 
