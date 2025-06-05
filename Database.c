c0d8a3da3776c80f7edb2ea960528f4af722a8af
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-06-05 12:57:55 -0600

    implemented strtok methods

--- Database.c
+++ Database.c
@@ -26,7 +26,7 @@ typedef enum {
 
 }MetaCommandResult;
 
-typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATMENT, PREPARE_SYNTAX_ERROR}PrepareResult; 
+typedef enum {PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATMENT, PREPARE_SYNTAX_ERROR, PREPARE_STRING_TOO_LONG}PrepareResult; 
 
 typedef enum {STATEMENT_INSERT, STATEMENT_SELECT}StatementType;
 
@@ -34,10 +34,11 @@ typedef enum {EXECUTE_SUCCESS , EXECUTE_TABLE_FULL}ExecuteResult;
 
 #define COLUMN_USERNAME_SIZE 32
 #define COLUMN_EMAIL_SIZE 255
+
 typedef struct {
     uint32_t id;
-    char username[COLUMN_USERNAME_SIZE];
-    char email[COLUMN_EMAIL_SIZE];
+    char username[COLUMN_USERNAME_SIZE + 1];
+    char email[COLUMN_EMAIL_SIZE + 1];
 }Row;
 
 
@@ -148,21 +149,50 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table){
     }
 }
 
-PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
-    if (strncmp(input_buffer->buffer, "insert", 6) == 0){
-        statement->type = STATEMENT_INSERT; // currently only allows key words , going to make it accept sentences
-        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+// replaceing scanf with strtok (string token) because it breaks up strings into small parts called 
+// tokens. if we use scanf and move on we will cause a buffer overflow due to the the buffer size of scanf
+// if the string is larger than the buffer than it will cause buffer overflow, strtok will break up a string into smaller 
+// tokens and store in the buffer. 
 
-        if (args_assigned < 3){
-            return PREPARE_SYNTAX_ERROR;
-        }
-        return PREPARE_SUCCESS;
+PrepareResult prepare_insert(InputBuffer* input_buffer, Statement* statement){
+
+    statement->type = STATEMENT_INSERT; 
+
+    char* keyword = strtok(input_buffer->buffer , " ");
+    char* id_string = strtok(NULL, " ");
+    char* username = strtok(NULL, " "); 
+    char* email = strtok(NULL, " "); 
+
+    if (id_string == NULL || username == NULL || email == NULL){
+        return PREPARE_SYNTAX_ERROR;
     }
+
+    int id = atoi(id_string); // atoi() converts a string into an interger 
+    if (strlen(username) > COLUMN_USERNAME_SIZE){
+        return PREPARE_STRING_TOO_LONG;
+    }
+    if (strlen(email) > COLUMN_EMAIL_SIZE){
+        return PREPARE_STRING_TOO_LONG; 
+    }
+
+    statement->row_to_insert.id = id;
+    strcpy(statement->row_to_insert.username, username); 
+    strcpy(statement->row_to_insert.email, email);
+
+    return PREPARE_SUCCESS; 
+
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0){
+        return prepare_insert(input_buffer, statement);
+    }
+
     if(strcmp(input_buffer->buffer, "select") == 0){
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
     }
-
     return PREPARE_UNRECOGNIZED_STATMENT;
 
 }
