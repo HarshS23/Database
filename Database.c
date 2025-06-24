77daf84f0f02fec9435f07455a934d0d0c61201b
Adharsh Shokkalingam <Harsh.shokka@gmail.com>
2025-06-24 15:25:59 -0600

    header files

--- Database.c
+++ Database.c
@@ -1,8 +1,11 @@
+#include <errno.h>
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
+#include <unistd.h>
 
 /*
 Right now we are making a simple REPL 
